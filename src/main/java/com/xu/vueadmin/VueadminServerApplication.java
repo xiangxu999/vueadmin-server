935b89e134a8f36ba68dc866c02954d5c92075b5
文享旭 <846212939@qq.com>
2021-10-29 20:47:13 +0800

    Initial commit

---
+++ src/main/java/com/xu/vueadmin/VueadminServerApplication.java
@@ -0,0 +1,13 @@
+package com.xu.vueadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueadminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueadminServerApplication.class, args);
+    }
+
+}
