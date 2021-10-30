603a6c3e83183544f11592f13c6d9ba329c84bfa
文享旭 <846212939@qq.com>
2021-10-30 11:17:18 +0800

    整合SpringSecurity、jwt、redis

---
+++ src/main/java/com/xu/vueadmin/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.xu.vueadmin.exception;
+
+import com.xu.vueadmin.common.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+
+/**
+ * Description 全局异常处理
+ * Date 2021/10/30 10:04
+ * Version 1.0.1
+ *
+ * @author Wen
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public Result handler(MethodArgumentNotValidException e) {
+        log.info("实体校验异常：----------------{}", e.getMessage());
+        BindingResult bindingResult = e.getBindingResult();
+        ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
+        return Result.fail(objectError.getDefaultMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public Result handler(IllegalArgumentException e) {
+        log.error("Assert异常：----------------{}", e.getMessage());
+        return Result.fail(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = RuntimeException.class)
+    public Result handler(RuntimeException e) {
+        log.error("运行时异常：----------------{}", e);
+        return Result.fail(e.getMessage());
+    }
+}
